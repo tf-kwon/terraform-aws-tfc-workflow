e2a455158bc548feafdbc9a4acb6ebe4dafd5336
kwon.hyeokcheol <kwon.hyeokcheol@tech-flag.co.jp>
2026-04-02 11:29:55 +0900

    Add support for multiple EC2 instances and update output values

--- main.tf
+++ main.tf
@@ -121,16 +121,20 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_eip" "hashicat" {
-  instance = aws_instance.hashicat.id
+  count    = var.ec2_count
+  instance = aws_instance.hashicat[count.index].id
   domain   = "vpc"
+  vpc      = true
 }
 
 resource "aws_eip_association" "hashicat" {
-  instance_id   = aws_instance.hashicat.id
-  allocation_id = aws_eip.hashicat.id
+  count         = var.ec2_count
+  instance_id   = aws_instance.hashicat[count.index].id
+  allocation_id = aws_eip.hashicat[count.index].id
 }
 
 resource "aws_instance" "hashicat" {
+  count                       = var.ec2_count
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
@@ -156,6 +160,7 @@ resource "aws_instance" "hashicat" {
 # Add execute permissions to our scripts.
 # Run the deploy_app.sh script.
 resource "null_resource" "configure_cat_app" {
+  count      = var.ec2_count
   depends_on = [aws_eip_association.hashicat]
 
   // triggers = {
@@ -170,7 +175,7 @@ resource "null_resource" "configure_cat_app" {
       type        = "ssh"
       user        = "ubuntu"
       private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
+      host        = aws_eip.hashicat[count.index].public_ip
     }
   }
 
@@ -192,7 +197,7 @@ resource "null_resource" "configure_cat_app" {
       type        = "ssh"
       user        = "ubuntu"
       private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
+      host        = aws_eip.hashicat[count.index].public_ip
     }
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
+  value = [for eip in aws_eip.hashicat : "http://${eip.public_dns}"]
 }
 
 output "catapp_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
+  value = [for eip in aws_eip.hashicat : "http://${eip.public_ip}"]
 }
+
